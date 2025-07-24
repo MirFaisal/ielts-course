fd2972e7dd7eb582ddf87eb341a4055728371032
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-07-25 00:04:47 +0600

    fix: add a space after Navbar component in layout.tsx for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${hindSiliguri.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar />
+        <Navbar /> 
         {children}
       </body>
     </html>
