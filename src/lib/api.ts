23961e2fce1efac6fcc7fc29188afb20a361dd88
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-07-24 21:05:41 +0600

    Initial commit for ielts-course project

---
+++ src/lib/api.ts
@@ -0,0 +1,14 @@
+export async function fetchProduct(lang: "en" | "bn") {
+  const res = await fetch(
+    `https://api.10minuteschool.com/discovery-service/api/v1/products/ielts-course?lang=${lang}`,
+    {
+      headers: {
+        "X-TENMS-SOURCE-PLATFORM": "web",
+        accept: "application/json",
+      },
+      cache: "no-store",
+    },
+  );
+  if (!res.ok) throw new Error("Failed to fetch product");
+  return res.json();
+}
