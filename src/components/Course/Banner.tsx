84819d9e410be0fcc1443974da401a8b0dcca702
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-07-27 20:53:23 +0600

    fix: Refactor Banner component for improved layout and styling consistency

--- src/components/Course/Banner.tsx
+++ src/components/Course/Banner.tsx
@@ -5,18 +5,30 @@ export default function Banner() {
   const [isShown, setIsShown] = useState(true);
   return (
     isShown && (
-      <div className="relative">
-        <div className="relative w-full h-[150px]">
+      <div className="relative w-full">
+        {/* Desktop Banner */}
+        <div className="relative w-full h-[150px] hidden lg:block">
           <Image
             src="https://cdn.10minuteschool.com/images/dasktop_banner_1753270611489.png"
             alt="Banner Image"
             fill
-            style={{ objectFit: "cover" }}
+            className="object-cover object-center"
+            priority
+          />
+        </div>
+        {/* Mobile Banner */}
+        <div className="relative w-full h-[80px] md:h-[130px] lg:hidden">
+          <Image
+            src="https://cdn.10minuteschool.com/images/mobile_banner_1753355543677.png"
+            alt="Banner Image"
+            fill
+            className="object-cover object-center"
+            priority
           />
         </div>
         <button
           onClick={() => setIsShown(false)}
-          className="absolute top-2 right-2 text-white p-1 transition-colors">
+          className="absolute top-1 right-1 md:top-2 md:right-2 z-10 text-white md:p-2 ">
           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
